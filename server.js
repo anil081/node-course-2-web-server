60c644b15210138364f2c8ca2847a3488282a3a3
anil081 <anil.yadav081@gmail.com>
2018-07-22 11:06:04 +0530

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
@@ -16,10 +17,7 @@ fs.appendFile('server.log',log + '\n');
 
  next();
 });
-// app.use((req, res, next) => {
-// 	res.render('maintenance.hbs');
-
-// });
+//  
 app.use(express.static(__dirname + '/public'));
 hbs.registerHelper('getCurrentYear', () => {
 	return new Date().getFullYear();
@@ -53,6 +51,6 @@ app.get('/bad', (req, res) => {
 res.send({ errorMessage: 'unable to fetch data'})
 });
 
-app.listen(3000, () => {
-	console.log('Server is up on port 3000')
+app.listen(port, () => {
+	console.log(`Server is up on port ${port}`);
 });
